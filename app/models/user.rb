b2ed6e36584adbccecf911a03b40a69de7129fd3
jhonatan <jhonatan_goncalves@id.uff.br>
2019-02-12 00:04:41 -0200

    resolvendo conflito

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   
-  mount_uploader :photo, PhotoUploader
 
   belongs_to :board
   belongs_to :cell
