6931306b0e7c3fe7018096ad81b1e920177777a9
Arthur de Lima Nakao <arthur.nakao@id.uff.br>
2019-02-12 14:50:27 -0200

    Permissões Possiveis até o momento prontas

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,15 +8,47 @@ class Ability
     end
       
     if user.accessor?
-
+      #Permissões de acessor (Herança)
+      can [:read], [Board, Cell, Goal, Post, Project, User] #read Padrão
+      can [:manage], Like #Dar Like
+      can [:manage], Dislike #Dar Deslike
+      can [:create], Post #Publicar artigos e perguntas 
+      #can [:create], Comment #Responder Perguntas
+      #can [:search], Post #Pesquisar Artigo
+      #can [:read], Task (Que ele está incluso) #Dar check nas tarefas do sistema de tarefas
+      #Pode exportar artigo (PDF)
+    
+      #Final das Permissões de acessor (Herança)
     end
 
     if user.director?
+      #Permissões de Diretor (Herança)
 
+      can [:manage], [Cell, Post, Board]
+      can [:manage], Goal, board_id: user.board_id
+      #Deve ser adicionado ao diretor poder gerenciar tarefas, mas não é no CanCanCan
+
+      #Diretor de Gestão Pessoas
+      if user.board_id == Board.find_by(name: "Gestão de Pessoas").id
+        can [:manage], User, [board_kind: 1, board_id: Board.find_by(name: "Gestão de Pessoas").id]
+        can [:manage], Fusion
+      end
+
+      #Final das Permissões de Diretor (Herança)
     end
 
     if user.manager?
+      #Permissões do Gerente da Célula (Herança)
 
+      can [:manage], Cell
+      can [:manage], Goal, cell_id: user.cell_id
+      #Deve ser adicionado ao gerente poder gerenciar tarefas, mas não é no CanCanCan
+      
+      if user.cell_id == Cell.find_by(name: "PMO")
+        can [:manage], Project
+      end
+
+      #Final das Permissões do Gerente da Célula (Herança)
     end
 
     if user.member?
