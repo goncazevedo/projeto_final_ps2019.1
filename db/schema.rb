f24b4db63fe8544a5b2a463eb0721ac44e05ab28
Juliana Martins de Moura <juliana_moura@id.uff.br>
2019-02-13 14:00:56 -0200

    Login raponsivo e código limpo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_214924) do
+ActiveRecord::Schema.define(version: 2019_02_12_233840) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
