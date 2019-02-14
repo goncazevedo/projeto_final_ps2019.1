7287a6f19b81477f6dfae402704e316c3119e662
Arthur de Lima Nakao <arthur.nakao@id.uff.br>
2019-02-14 16:46:56 -0200

    Validaçao das Metas

--- app/models/goal_board.rb
+++ app/models/goal_board.rb
@@ -1,4 +1,6 @@
 class GoalBoard < ApplicationRecord
   belongs_to :board
   has_many :task_boards, dependent: :destroy
+
+  validates :name, :deadline, :board_id, presence: true
 end
--- app/models/goal_cell.rb
+++ app/models/goal_cell.rb
@@ -1,4 +1,6 @@
 class GoalCell < ApplicationRecord
   belongs_to :cell
   has_many :task_cells, dependent: :destroy
+
+  validates :name, :deadline, :cell_id, presence: true
 end
