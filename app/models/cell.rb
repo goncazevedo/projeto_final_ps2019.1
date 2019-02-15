c04f0e220d192f318e92a9bd6ed05c61a06069df
Arthur de Lima Nakao <arthur.nakao@id.uff.br>
2019-02-15 15:35:32 -0200

    Adicionando Celulas dentro do show da diretoria

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -2,5 +2,7 @@ class Cell < ApplicationRecord
     has_many :goal_cells, dependent: :destroy
     has_many :users
 
-    validates :name, :description, board_id, presence: true
+    belongs_to :board
+
+    validates :name, :description, presence: true
 end
