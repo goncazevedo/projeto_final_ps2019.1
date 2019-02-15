1f39f7a0a0df876c36ce3aa777a59fd90fb7614d
Arthur de Lima Nakao <arthur.nakao@id.uff.br>
2019-02-15 15:07:17 -0200

    Adicionando board_id a entidade celula e validaçao desse board_id

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -2,5 +2,5 @@ class Cell < ApplicationRecord
     has_many :goal_cells, dependent: :destroy
     has_many :users
 
-    validates :name, :description, presence: true
+    validates :name, :description, board_id, presence: true
 end
