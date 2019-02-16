9812216c66fcc0cdad4fc9d3dd683c647c0ba3e8
Arthur de Lima Nakao <arthur.nakao@id.uff.br>
2019-02-16 03:09:50 -0200

    Logica para fazer com que a meta seja finalizada automaticamente completa para a celula tambem e bug de marcar como finalizado quando a ultima tarefa foi criada como finalizado resolvido

--- app/controllers/task_boards_controller.rb
+++ app/controllers/task_boards_controller.rb
@@ -30,7 +30,7 @@ class TaskBoardsController < ApplicationController
 
     respond_to do |format|
       if @task_board.save
-        goal_finished
+        goal_finished #Função para fazer com que a meta seja finalizada automaticamente quando todas as tarefas foram finalizadas
         format.html { redirect_to @task_board, notice: 'Task board was successfully created.' }
         format.json { render :show, status: :created, location: @task_board }
       else
@@ -45,7 +45,7 @@ class TaskBoardsController < ApplicationController
   def update
     respond_to do |format|
       if @task_board.update(task_board_params)
-        goal_finished
+        goal_finished #Função para fazer com que a meta seja finalizada automaticamente quando todas as tarefas foram finalizadas
         format.html { redirect_to @task_board, notice: 'Task board was successfully updated.' }
         format.json { render :show, status: :ok, location: @task_board }
       else
@@ -76,18 +76,18 @@ class TaskBoardsController < ApplicationController
       params.require(:task_board).permit(:name, :description, :finished, :goal_board_id)
     end
 
-    def goal_finished
+    def goal_finished #Função para fazer com que a meta seja finalizada automaticamente quando todas as tarefas foram finalizadas
+      @tasks_finisheds = true
       @task_board.goal_board.task_boards.each do |task|
-        tasks_finisheds = true
         if task.finished == false
-          tasks_finisheds = false
-        end
-
-        if tasks_finisheds == true
-          @task_board.goal_board.update(name: @task_board.goal_board.name, deadline: @task_board.goal_board.deadline, finished: true, board_id: @task_board.goal_board.board_id)
-        else
-          @task_board.goal_board.update(name: @task_board.goal_board.name, deadline: @task_board.goal_board.deadline, finished: false, board_id: @task_board.goal_board.board_id)
+          @tasks_finisheds = false
         end
       end
+      if @tasks_finisheds == true
+        @task_board.goal_board.update(name: @task_board.goal_board.name, deadline: @task_board.goal_board.deadline, finished: true, board_id: @task_board.goal_board.board_id)
+      else
+        @task_board.goal_board.update(name: @task_board.goal_board.name, deadline: @task_board.goal_board.deadline, finished: false, board_id: @task_board.goal_board.board_id)
+      end
     end
+
 end
